428ab0e4ac72112b64a3ed5c63ea04dfa6549f85
chiahao3 <harrey810719@gmail.com>
2024-04-23 16:15:30 -0400

    fix: imshift_batch was missing a ) so the shifts are not applied correctly. It has very little effect when the sub-px shifts are <<1, but it could be limiting the capability of pos correction for the previous version

--- ptyrad/inputs/params_CNS.py
+++ ptyrad/inputs/params_CNS.py
@@ -11,16 +11,16 @@ exp_params = {
     'dx_spec'           : 0.1406,# Ang, used to calculate dk
     'defocus'           : -100, # Ang, positive defocus here refers to actual underfocus or weaker lens strength following Kirkland/abtem/ptychoshelves convention
     'c3'                : 0, # Ang, spherical aberration coefficients
-    'z_distance'        : 1, # Ang
-    'Nlayer'            : 10,
+    'z_distance'        : 10, # Ang
+    'Nlayer'            : 1,
     'N_scans'           : 7134,
     'N_scan_slow'       : 87,
     'N_scan_fast'       : 82,
     'scan_step_size'    : 0.28, # Ang
-    'scan_flip'         : (2), # (2) for 'simu' pos, None for loaded pos. Modify scan_flip would change the image orientation.
-    'scan_affine'       : None, # (scale, asymmetry, rotation, shear)
+    'scan_flip'         : None, # (2) for 'simu' pos, None for loaded pos. Modify scan_flip would change the image orientation.
+    'scan_affine'       : (1,0,2,0), # (scale, asymmetry, rotation, shear)
     'omode_max'         : 1, #1
-    'pmode_max'         : 1, #2
+    'pmode_max'         : 2, #2
     'pmode_init_pows'   : [0.02],
     'probe_permute'     : None,
     'cbeds_permute'     : (0,1,3,2),
@@ -33,18 +33,18 @@ exp_params = {
 source_params = {
     'measurements_source': 'mat',
     'measurements_params': [exp_CBED_path, 'cbed'],
-    'obj_source'         : 'simu',
-    'obj_params'         : None, # (1,8,391,403),
+    # 'obj_source'         : 'simu',
+    # 'obj_params'         : None, # (1,8,391,403),
     'probe_source'       : 'simu',
     'probe_params'       : None, 
-    'pos_source'         : 'simu',
-    'pos_params'         : None,
-    # 'obj_source'         : 'PtyShv', 
-    # 'obj_params'         : ptycho_output_path, 
+    # 'pos_source'         : 'simu',
+    # 'pos_params'         : None,
+    'obj_source'         : 'PtyShv', 
+    'obj_params'         : ptycho_output_path, 
     # 'probe_source'       : 'PtyShv',
     # 'probe_params'       : ptycho_output_path, 
-    # 'pos_source'         : 'PtyShv',
-    # 'pos_params'         : ptycho_output_path,
+    'pos_source'         : 'PtyShv',
+    'pos_params'         : ptycho_output_path,
     'omode_occu_source'  : 'uniform',
     'omode_occu_params'  : None
 }
