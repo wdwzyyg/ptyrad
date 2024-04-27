df5c40a9c5e2309a26364e446d874d030189d758
chiahao3 <harrey810719@gmail.com>
2024-04-27 10:18:51 -0400

    minor change in __init__ for version printing

--- ptyrad/__init__.py
+++ ptyrad/__init__.py
@@ -1,6 +1,5 @@
-print("Importing from ptyrad")
-
 # __init__.py is not strictly needed since Python 3.3 for the intepreter to recognize it as a package 
 # However, this is still included for clarity and for initialization purpose
 
-__version__ = "0.1.0-alpha2.6"
+__version__ = "0.1.0-alpha2.6" # 2024.04.27
+print(f"Importing ptyrad {__version__}")
