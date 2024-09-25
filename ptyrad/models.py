Rewind the fixed precision attempt; Implement the accelerate-enabled mixed precision and make it a CLI argument for simplicity

ptyrad/models.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 from torchvision.transforms.functional import gaussian_blur
 
 from ptyrad.forward import multislice_forward_model_vec_all
-from ptyrad.utils import add_tilts_to_propagator, get_compatible_complex_dtype, imshift_batch, str_to_dtype, vprint
+from ptyrad.utils import add_tilts_to_propagator, imshift_batch, vprint
 
 # The obj_ROI_grid is modified from precalculation to on-the-fly generation for memory consumption
 # It has very little performance impact but saves lots of memory for large 4D-STEM data
@@ -78,12 +78,10 @@ class PtychoAD(torch.nn.Module):
             Performs the forward pass and computes diffraction patterns for given indices.
     """
 
-    def __init__(self, init_variables, model_params, base_precision_str='bfloat16', device='cuda', verbose=True):
+    def __init__(self, init_variables, model_params, device='cuda', verbose=True):
         super(PtychoAD, self).__init__()
         with torch.no_grad():
             # Setup model behaviors
-            self.base_precision_type    = str_to_dtype(base_precision_str)
-            self.cplx_precision_type    = get_compatible_complex_dtype(base_precision_str)
             self.device                 = device
             self.verbose                = verbose
             self.detector_blur_std      = model_params['detector_blur_std']
@@ -100,29 +98,29 @@ class PtychoAD(torch.nn.Module):
             self.lr_params              = lr_dict
             
             # Optimizable parameters
-            self.opt_obja               = nn.Parameter(torch.abs(torch.tensor(init_variables['obj'],    device=device)).to(self.base_precision_type))
-            self.opt_objp               = nn.Parameter(torch.angle(torch.tensor(init_variables['obj'],  device=device)).to(self.base_precision_type))
-            self.opt_obj_tilts          = nn.Parameter(torch.tensor(init_variables['obj_tilts'],                dtype=self.base_precision_type, device=device))
-            self.opt_probe              = nn.Parameter(torch.view_as_real(torch.tensor(init_variables['probe'], dtype=self.cplx_precision_type, device=device))) # The `torch.view_as_real` allows correct handling of DDP via NCCL even in PyTorch 2.4
-            self.opt_probe_pos_shifts   = nn.Parameter(torch.tensor(init_variables['probe_pos_shifts'],         dtype=self.base_precision_type, device=device))
+            self.opt_obja               = nn.Parameter(torch.abs(torch.tensor(init_variables['obj'],    device=device)).to(torch.float32))
+            self.opt_objp               = nn.Parameter(torch.angle(torch.tensor(init_variables['obj'],  device=device)).to(torch.float32))
+            self.opt_obj_tilts          = nn.Parameter(torch.tensor(init_variables['obj_tilts'],                dtype=torch.float32, device=device))
+            self.opt_probe              = nn.Parameter(torch.view_as_real(torch.tensor(init_variables['probe'], dtype=torch.complex64, device=device))) # The `torch.view_as_real` allows correct handling of DDP via NCCL even in PyTorch 2.4
+            self.opt_probe_pos_shifts   = nn.Parameter(torch.tensor(init_variables['probe_pos_shifts'],         dtype=torch.float32, device=device))
             
             # Buffers are used during forward pass
-            self.register_buffer      ('omode_occu',   torch.tensor(init_variables['omode_occu'],       dtype=self.base_precision_type, device=device))
-            self.register_buffer      ('H',            torch.tensor(init_variables['H'],                dtype=self.cplx_precision_type, device=device))
-            self.register_buffer      ('measurements', torch.tensor(init_variables['measurements'],     dtype=self.base_precision_type, device=device))
-            self.register_buffer      ('N_scan_slow',  torch.tensor(init_variables['N_scan_slow'],      dtype=torch.int16, device=device))# Saving this for reference, the cropping is based on self.obj_ROI_grid.
-            self.register_buffer      ('N_scan_fast',  torch.tensor(init_variables['N_scan_fast'],      dtype=torch.int16, device=device))# Saving this for reference, the cropping is based on self.obj_ROI_grid.
-            self.register_buffer      ('crop_pos',     torch.tensor(init_variables['crop_pos'],         dtype=torch.int16, device=device))# Saving this for reference, the cropping is based on self.obj_ROI_grid.
-            self.register_buffer      ('z_distance',   torch.tensor(init_variables['z_distance'],       dtype=self.base_precision_type, device=device))# Saving this for reference
-            self.register_buffer      ('dx',           torch.tensor(init_variables['dx'],               dtype=self.base_precision_type, device=device))# Saving this for reference
-            self.register_buffer      ('dk',           torch.tensor(init_variables['dk'],               dtype=self.base_precision_type, device=device))# Saving this for reference
+            self.register_buffer      ('omode_occu',   torch.tensor(init_variables['omode_occu'],       dtype=torch.float32, device=device))
+            self.register_buffer      ('H',            torch.tensor(init_variables['H'],                dtype=torch.complex64, device=device))
+            self.register_buffer      ('measurements', torch.tensor(init_variables['measurements'],     dtype=torch.float32, device=device))
+            self.register_buffer      ('N_scan_slow',  torch.tensor(init_variables['N_scan_slow'],      dtype=torch.int32, device=device))# Saving this for reference, the cropping is based on self.obj_ROI_grid.
+            self.register_buffer      ('N_scan_fast',  torch.tensor(init_variables['N_scan_fast'],      dtype=torch.int32, device=device))# Saving this for reference, the cropping is based on self.obj_ROI_grid.
+            self.register_buffer      ('crop_pos',     torch.tensor(init_variables['crop_pos'],         dtype=torch.int32, device=device))# Saving this for reference, the cropping is based on self.obj_ROI_grid.
+            self.register_buffer      ('z_distance',   torch.tensor(init_variables['z_distance'],       dtype=torch.float32, device=device))# Saving this for reference
+            self.register_buffer      ('dx',           torch.tensor(init_variables['dx'],               dtype=torch.float32, device=device))# Saving this for reference
+            self.register_buffer      ('dk',           torch.tensor(init_variables['dk'],               dtype=torch.float32, device=device))# Saving this for reference
 
             self.scan_affine            = init_variables['scan_affine']                                                           # Saving this for reference
             self.tilt_obj               = self.lr_params['obj_tilts']        != 0 or torch.any(self.opt_obj_tilts)                # Set tilt_obj to True if lr_params['obj_tilts'] is not 0 or we have any none-zero tilt values
             self.shift_probes           = self.lr_params['probe_pos_shifts'] != 0                                                 # Set shift_probes to True if lr_params['probe_pos_shifts'] is not 0
             self.probe_int_sum          = self.get_complex_probe_view().abs().pow(2).sum()
             
-            # Create grids for shifting the probe
+            # Create grids for shifting
             self.create_grids()
 
             # Create a dictionary to store the optimizable tensors
@@ -133,7 +131,6 @@ class PtychoAD(torch.nn.Module):
                 'probe'           : self.opt_probe,
                 'probe_pos_shifts': self.opt_probe_pos_shifts}
             self.create_optimizable_params_dict(self.lr_params, self.verbose)
-            self.half()
     
     def get_complex_probe_view(self):
         """ Retrieve complex view of the probe """
@@ -156,7 +153,7 @@ class PtychoAD(torch.nn.Module):
                                   torch.arange(Nox, dtype=torch.int32, device=device), indexing='ij') # real space grid for object in y and x directions
         
         self.shift_probes_grid = torch.stack([rpy/Npy, rpx/Npx], dim=0) # (2,Npy,Npx), normalized k-space grid stack for sub-px probe shifting 
-        self.shift_object_grid = torch.stack([roy/Noy, rox/Nox], dim=0) # (2,Noy,Nox), normalized k-space grid stack for sub-px object shifting
+        self.shift_object_grid = torch.stack([roy/Noy, rox/Nox], dim=0) # (2,Noy,Nox), normalized k-space grid stack for sub-px object shifting (Implemented for completeness, not used in PtyRAD)
         
         self.rpy_grid = rpy # real space grid with y-indices spans across probe extent
         self.rpx_grid = rpx

ptyrad/optimization.py
@@ -7,7 +7,7 @@ from torch.nn.functional import interpolate
 from torchvision.transforms.functional import gaussian_blur
 
 from ptyrad.data_io import load_pt
-from ptyrad.utils import fftshift2, gaussian_blur_1d, str_to_dtype, ifftshift2, make_sigmoid_mask, vprint
+from ptyrad.utils import fftshift2, gaussian_blur_1d, ifftshift2, make_sigmoid_mask, vprint
 
 # The CombinedLoss takes a user-defined dict of loss_params, which specifies the state, weight, and param of each loss term
 # The DP related loss takes a parameter of dp_pow which raise the DP with certain power, 
@@ -46,13 +46,11 @@ class CombinedLoss(torch.nn.Module):
         forward(model_DP, measured_DP, object_patches, omode_occu):
             Combines all the loss components and returns the total loss and individual losses.
     """
-    def __init__(self, loss_params, base_precision_str='float32', device='cuda'):
+    def __init__(self, loss_params, device='cuda'):
         super(CombinedLoss, self).__init__()
-        self.base_precision_type = str_to_dtype(base_precision_str)
         self.device = device
         self.loss_params = loss_params
         self.mse = torch.nn.MSELoss(reduction='mean')
-        self.half()
 
     def get_loss_single(self, model_DP, measured_DP):
         # Calculate loss_single
@@ -66,7 +64,7 @@ class CombinedLoss(torch.nn.Module):
             loss_single = self.mse(model_DP.pow(dp_pow), measured_DP.pow(dp_pow))**0.5 / data_mean # Doing Normalized RMSE makes the value quite consistent between dp_pow 0.2-0.5.
             loss_single *= single_params['weight']
         else:
-            loss_single = torch.tensor(0, dtype=self.base_precision_type, device=self.device) # Return a scalar 0 tensor so that the append/sum would work normally without NaN
+            loss_single = torch.tensor(0, dtype=torch.float32, device=self.device) # Return a scalar 0 tensor so that the append/sum would work normally without NaN
         return loss_single
     
     def get_loss_poissn(self, model_DP, measured_DP):
@@ -90,7 +88,7 @@ class CombinedLoss(torch.nn.Module):
             loss_poissn = -torch.mean(measured_DP.pow(dp_pow) * torch.log(model_DP.pow(dp_pow) + eps) - model_DP.pow(dp_pow)) / data_mean # Doing Normalized RMSE makes the value quite consistent between dp_pow 0.2-0.5.
             loss_poissn *= poissn_params['weight']
         else:
-            loss_poissn = torch.tensor(0, dtype=self.base_precision_type, device=self.device) # Return a scalar 0 tensor so that the append/sum would work normally without NaN
+            loss_poissn = torch.tensor(0, dtype=torch.float32, device=self.device) # Return a scalar 0 tensor so that the append/sum would work normally without NaN
         return loss_poissn
     
     def get_loss_pacbed(self, model_DP, measured_DP):
@@ -101,7 +99,7 @@ class CombinedLoss(torch.nn.Module):
             loss_pacbed = self.mse(model_DP.mean(0).pow(dp_pow), measured_DP.mean(0).pow(dp_pow))**0.5
             loss_pacbed *= pacbed_params['weight']
         else:
-            loss_pacbed = torch.tensor(0, dtype=self.base_precision_type, device=self.device)
+            loss_pacbed = torch.tensor(0, dtype=torch.float32, device=self.device)
         return loss_pacbed
         
     def get_loss_sparse(self, objp_patches, omode_occu):
@@ -115,7 +113,7 @@ class CombinedLoss(torch.nn.Module):
             ln_order = sparse_params['ln_order']
             loss_sparse = sparse_params['weight'] * (torch.mean(objp_patches.abs().pow(ln_order), dim=(0,2,3,4)).pow(1/ln_order) * omode_occu).sum()
         else:
-            loss_sparse = torch.tensor(0, dtype=self.base_precision_type, device=self.device)
+            loss_sparse = torch.tensor(0, dtype=torch.float32, device=self.device)
         return loss_sparse
     
     def get_loss_simlar(self, object_patches, omode_occu):
@@ -129,7 +127,7 @@ class CombinedLoss(torch.nn.Module):
             scale_factor = simlar_params['scale_factor']
             obja_patches = object_patches[...,0]
             objp_patches = object_patches[...,1]
-            temp_loss = torch.tensor(0, dtype=self.base_precision_type, device=self.device)
+            temp_loss = torch.tensor(0, dtype=torch.float32, device=self.device)
             
             if obj_type in ['amplitude', 'both']:
                 if obj_blur_std is not None and obj_blur_std != 0:
@@ -150,7 +148,7 @@ class CombinedLoss(torch.nn.Module):
                 temp_loss += (objp_patches * omode_occu[:,None,None,None]).std(1).mean()
             loss_simlar = simlar_params['weight'] * temp_loss
         else:
-            loss_simlar = torch.tensor(0, dtype=self.base_precision_type, device=self.device)
+            loss_simlar = torch.tensor(0, dtype=torch.float32, device=self.device)
         return loss_simlar
     
     def forward(self, model_DP, measured_DP, object_patches, omode_occu):
@@ -216,7 +214,6 @@ class CombinedConstraint(torch.nn.Module):
         self.device = device
         self.constraint_params = constraint_params
         self.verbose = verbose
-        self.half()
 
     def apply_ortho_pmode(self, model, niter):
         ''' Apply orthogonality constraint to probe modes '''
@@ -224,8 +221,8 @@ class CombinedConstraint(torch.nn.Module):
         if ortho_pmode_freq is not None and niter % ortho_pmode_freq == 0:
             model.opt_probe.data = torch.view_as_real(orthogonalize_modes_vec(model.get_complex_probe_view(), sort=True).contiguous()) # Note that model stores the complex probe as a (pmode, Ny, Nx, 2) float tensor (real view) so we need to do some real-complex view conversion.
             probe_int = model.get_complex_probe_view().abs().pow(2)
-            probe_pow = (probe_int.sum((1,2))/probe_int.sum())
-            vprint(f"Apply ortho pmode constraint at iter {niter}, relative pmode power = {probe_pow:.3f}, probe int sum = {probe_int.sum():.4f}", verbose=self.verbose)
+            probe_pow = (probe_int.sum((1,2))/probe_int.sum()).detach().cpu().numpy().round(3)
+            vprint(f"Apply ortho pmode constraint at iter {niter}, relative pmode power = {probe_pow}, probe int sum = {probe_int.sum():.4f}", verbose=self.verbose)
 
     def apply_probe_mask_k(self, model, niter):
         ''' Apply probe amplitude constraint in Fourier space '''
@@ -244,7 +241,7 @@ class CombinedConstraint(torch.nn.Module):
             powers = probe.abs().pow(2).sum((-2,-1)) / probe.abs().pow(2).sum()
             powers_cumsum = powers.cumsum(0)
             pmode_index = (powers_cumsum > power_thresh).nonzero()[0].item() # This gives the pmode index that the cumulative power along mode dimension is greater than the power_thresh and should have mask extend to this index
-            mask = torch.ones_like(probe, dtype=self.base_precision_type, device=model.device)
+            mask = torch.ones_like(probe, dtype=torch.float32, device=model.device)
             mask_value = make_sigmoid_mask(Npix, relative_radius, relative_width).to(model.device)
             mask[:pmode_index+1] = mask_value
             probe_k = fftshift2 (fft2(ifftshift2(probe), norm='ortho')) # probe_k at center for later masking

ptyrad/reconstruction.py
@@ -63,19 +63,13 @@ class PtyRADSolver(object):
             A wrapper method to run the solver in either reconstruction or hyperparameter 
             tuning mode based on the if_hypertune flag.
     """
-    def __init__(self, params, split_batches=True, mixed_precision_type='no'):
+    def __init__(self, params):
         self.params       = params
         self.if_hypertune = self.params['hypertune_params']['if_hypertune']
         self.verbose      = not self.params['recon_params']['if_quiet']
-        self.split_batches = split_batches
-        self.base_precision_type  = 'float16' # self.params['model_params']['base_precision_type']
-        self.mixed_precision_type = mixed_precision_type 
         
         # Set up accelerator for multiGPU setting, note that this has no effect when we launch it with just `python <script>`
-        dataloader_config = DataLoaderConfiguration(split_batches=self.split_batches) # This supress the warning when we do `Accelerator(split_batches=True)`
-        kwargs_handlers   = [DistributedDataParallelKwargs(find_unused_parameters=False)] # This avoids the error `RuntimeError: Expected to have finished reduction in the prior iteration before starting a new one. This error indicates that your module has parameters that were not used in producing loss.` We don't necessarily need this if we carefully register parameters (used in forward) and buffer in the `model`.
-        self.accelerator  = Accelerator(mixed_precision=self.mixed_precision_type, dataloader_config=dataloader_config, kwargs_handlers=kwargs_handlers)
-        self.accelerator.print(f"Accelerator DataLoader split_batches = {self.split_batches}")
+        self.init_accelerator()
         
         # model and optimizer are instantiate inside reconstruct() and hypertune()
         self.init_initializer()
@@ -86,6 +80,12 @@ class PtyRADSolver(object):
     @property
     def device(self):
         return self.accelerator.device
+    
+    def init_accelerator(self):
+        dataloader_config = DataLoaderConfiguration(split_batches=True) # This supress the warning when we do `Accelerator(split_batches=True)`
+        kwargs_handlers   = [DistributedDataParallelKwargs(find_unused_parameters=False)] # This avoids the error `RuntimeError: Expected to have finished reduction in the prior iteration before starting a new one. This error indicates that your module has parameters that were not used in producing loss.` We don't necessarily need this if we carefully register parameters (used in forward) and buffer in the `model`.
+        self.accelerator  = Accelerator(dataloader_config=dataloader_config, kwargs_handlers=kwargs_handlers)
+        self.accelerator.print(f"If launch with accelerate, mixed precision = {self.accelerator.mixed_precision}")
     
     def init_initializer(self):
         self.accelerator.print("\n### Initializing Initializer ###")
@@ -105,7 +105,7 @@ class PtyRADSolver(object):
         batch_size = params['recon_params']['BATCH_SIZE']['size']
         
         # Create the model and optimizer, prepare indices, batches, and output_path
-        model         = PtychoAD(self.init.init_variables, params['model_params'], base_precision_str=self.base_precision_type, device=device, verbose=self.verbose)
+        model         = PtychoAD(self.init.init_variables, params['model_params'], device=device, verbose=self.verbose)
         optimizer     = create_optimizer(model.optimizer_params, model.optimizable_params)
         indices, _, output_path = prepare_recon(model, self.init, params) # `batches` would not be used for the IndicedDataset because I haven't figured out how to do specified indices
         
@@ -239,8 +239,8 @@ def prepare_recon(model, init, params):
     if_hypertune = params['hypertune_params']['if_hypertune']
     
     # Generate the indices, batches, and fig_grouping
-    pos          = (model.crop_pos + model.opt_probe_pos_shifts).detach().to(torch.float32).cpu().numpy() # The .to(torch.float32) upcast is a preventive solution because .numpy() doesn't support bf16
-    probe_int    = model.get_complex_probe_view().abs().pow(2).sum(0).detach().to(torch.float32).cpu().numpy()
+    pos          = (model.crop_pos + model.opt_probe_pos_shifts).detach().cpu().numpy()
+    probe_int    = model.get_complex_probe_view().abs().pow(2).sum(0).detach().cpu().numpy()
     dx           = init_variables['dx']
     d_out        = get_blob_size(dx, probe_int, output='d90', verbose=verbose) # d_out unit is in Ang
     indices      = select_scan_indices(init_variables['N_scan_slow'], init_variables['N_scan_fast'], subscan_slow=subscan_slow, subscan_fast=subscan_fast, mode=INDICES_MODE, verbose=verbose)
@@ -378,14 +378,17 @@ def recon_step(batches, grad_accumulation, model, optimizer, loss_fn, constraint
     for batch_idx, batch in enumerate(batches):
         start_batch_t = time_sync()
         
-        model_DP, object_patches = model(batch)
-        measured_DP = model_instance.get_measurements(batch)
-        loss_batch, losses = loss_fn(model_DP, measured_DP, object_patches, model_instance.omode_occu)
-        
         if acc is not None:
+            with acc.autocast():
+                model_DP, object_patches = model(batch)
+                measured_DP = model_instance.get_measurements(batch)
+                loss_batch, losses = loss_fn(model_DP, measured_DP, object_patches, model_instance.omode_occu)
             acc.backward(loss_batch)
-        else:
-            loss_batch.backward() # This is kept when we run PtyRAD in the detailed walkthrough notebook without `accelerate`
+        else: # This is kept when we run PtyRAD in the detailed walkthrough notebook without launching through `accelerate`
+            model_DP, object_patches = model(batch)
+            measured_DP = model_instance.get_measurements(batch)
+            loss_batch, losses = loss_fn(model_DP, measured_DP, object_patches, model_instance.omode_occu)
+            loss_batch.backward() 
             
         # Perform the optimizer step when batch_idx + 1 is divisible by grad_accumulation or it's the last batch
         if (batch_idx + 1) % grad_accumulation == 0 or (batch_idx + 1) == len(batches):
@@ -398,7 +401,7 @@ def recon_step(batches, grad_accumulation, model, optimizer, loss_fn, constraint
         if acc is not None:
             acc.wait_for_everyone()
         for loss_name, loss_value in zip(loss_fn.loss_params.keys(), losses):
-            batch_losses[loss_name].append(loss_value.detach().to(torch.float32).cpu().numpy())
+            batch_losses[loss_name].append(loss_value.detach().cpu().numpy())
         if batch_idx in np.linspace(0, len(batches)-1, num=6, dtype=int):
             vprint(f"Done batch {batch_idx+1} with {len(batch)} indices ({batch[:5].tolist()}...) in {batch_t:.3f} sec", verbose=verbose)
     

ptyrad/visualization.py
@@ -10,7 +10,7 @@ def plot_sigmoid_mask(Npix, relative_radius, relative_width, img=None, show_circ
     """ Plot a sigmoid mask overlay on img with a line profile """
     # Note that relative_radius ranges from 0 - 1 for center -> edge. radius = 1 corresponds to a inscribed circle
     # While relative_width also ranges from 0 - 1 for Npix * relative_width. width = 0.05 corresponds to a width of 5% of the image width would have sigmoid value change from 0 - 1
-    mask = make_sigmoid_mask(Npix, relative_radius, relative_width).detach().to(torch.float32).cpu().numpy() # The .to(torch.float32) upcast is a preventive solution because .numpy() doesn't support bf16
+    mask = make_sigmoid_mask(Npix, relative_radius, relative_width).detach().cpu().numpy()
     img = np.ones((Npix,Npix)) if img is None else img/img.max()
     masked_img = mask * img
     fig, axs = plt.subplots(1,2, figsize=(13,6))
@@ -52,11 +52,11 @@ def plot_forward_pass(model, indices, dp_power, show_fig=True, pass_fig=False):
         omode_occu               = model.omode_occu
         measured_DP           = model.get_measurements(indices)
         
-        probes_int     = probes_int.detach().to(torch.float32).cpu().numpy() # The .to(torch.float32) upcast is a preventive solution because .numpy() doesn't support bf16
-        obja_ROI        = (obj_patches[...,0] * omode_occu[:,None,None,None]).sum(1).detach().to(torch.float32).cpu().numpy() # obj_ROI = (N_i, Nz,Ny,Nx)
-        objp_ROI        = (obj_patches[...,1] * omode_occu[:,None,None,None]).sum(1).detach().to(torch.float32).cpu().numpy() # obj_ROI = (N_i, Nz,Ny,Nx)
-        model_DP    = model_DP.detach().to(torch.float32).cpu().numpy()
-        measured_DP = measured_DP.detach().to(torch.float32).cpu().numpy()
+        probes_int     = probes_int.detach().cpu().numpy()
+        obja_ROI        = (obj_patches[...,0] * omode_occu[:,None,None,None]).sum(1).detach().cpu().numpy() # obj_ROI = (N_i, Nz,Ny,Nx)
+        objp_ROI        = (obj_patches[...,1] * omode_occu[:,None,None,None]).sum(1).detach().cpu().numpy() # obj_ROI = (N_i, Nz,Ny,Nx)
+        model_DP    = model_DP.detach().cpu().numpy()
+        measured_DP = measured_DP.detach().cpu().numpy()
     
     plt.ioff() # Temporaily disable the interactive plotting mode
     fig, axs = plt.subplots(len(indices), 5, figsize=(24, 5*len(indices)))
@@ -360,7 +360,7 @@ def plot_summary(output_path, model, loss_iters, niter, indices, init_variables,
     
     # Probe modes in real and reciprocal space
     init_probe = init_variables['probe']
-    opt_probe = model.get_complex_probe_view().detach().to(torch.float32).cpu().numpy() # The .to(torch.float32) upcast is a preventive solution because .numpy() doesn't support bf16
+    opt_probe = model.get_complex_probe_view().detach().cpu().numpy()
 
     if 'probe_r_amp' in selected_figs or 'all' in selected_figs:
         fig_probe_modes_real_amp      = plot_probe_modes(init_probe, opt_probe, real_or_fourier='real',    amp_or_phase='amplitude', show_fig=False, pass_fig=True)
@@ -391,8 +391,8 @@ def plot_summary(output_path, model, loss_iters, niter, indices, init_variables,
             
     # Scan positions and tilts
     init_pos = init_variables['crop_pos'] + init_variables['probe_pos_shifts']
-    pos = (model.crop_pos + model.opt_probe_pos_shifts).detach().to(torch.float32).cpu().numpy() # The .to(torch.float32) upcast is a preventive solution because .numpy() doesn't support bf16
-    tilts = model.opt_obj_tilts.detach().to(torch.float32).cpu().numpy()
+    pos = (model.crop_pos + model.opt_probe_pos_shifts).detach().cpu().numpy()
+    tilts = model.opt_obj_tilts.detach().cpu().numpy()
     tilts = np.broadcast_to(tilts, (len(pos), 2)) # tilts has to be (N_scan, 2)
     
     if 'pos' in selected_figs or 'all' in selected_figs: