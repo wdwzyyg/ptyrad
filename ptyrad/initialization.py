Fix `get_measurements`, `self.probe_int_sum`,  `init_measurements`, and `init_probe` for on-the-fly meas_padding. The previous commit calculates the intensity normalization constant (for `fix_probe_int` constraint) using `get_measurements()` durint PtychoAD initialization, which will consume too much GPU memory during on-the-fly padding/resampling. The normalization is now done during initialization by saving into a field. Update params and figure notebooks. Add a demo notebook for padding with fitted background.

ptyrad/initialization.py
@@ -260,8 +260,8 @@ class Initializer:
             mode         = self.init_params['exp_params']['meas_pad']['mode'] # 'on_the_fly' or 'precompute'
             padding_type = self.init_params['exp_params']['meas_pad']['padding_type']
             target_Npix  = self.init_params['exp_params']['meas_pad']['target_Npix']
-            value        = self.init_params['exp_params']['meas_pad']['value']
-            threshold    = self.init_params['exp_params']['meas_pad'].get('threshold', 90)
+            value        = self.init_params['exp_params']['meas_pad'].get('value', 10)
+            threshold    = self.init_params['exp_params']['meas_pad'].get('threshold', 70)
             meas_avg     = meas.mean(0)
             meas_int_sum = meas_avg.sum()
             amp_avg      = np.sqrt(meas_avg)
@@ -304,7 +304,7 @@ class Initializer:
                 background = power_law(r, *popt)
                 amp_padded = background
             else:
-                raise KeyError(f"meas_pad does not support padding_type = '{padding_type}', please choose from 'constant', 'edge', 'exp', or 'power'")
+                raise KeyError(f"meas_pad does not support padding_type = '{padding_type}', please choose from 'constant', 'edge', 'linear_ramp', 'exp', or 'power'")
 
             # Square the padded amplitude back to intensity
             meas_padded = np.square(amp_padded)[None,] # (1, ky, kx)
@@ -312,7 +312,7 @@ class Initializer:
             # Parse intensity information
             meas_padded[..., pad_h1:pad_h2, pad_w1:pad_w2] = 0
             padded_int_sum = meas_padded.sum()
-            vprint(f"Original meas int sum = {meas_int_sum:.4f}, padded region int sum = {padded_int_sum:.4f}, or {padded_int_sum/meas_int_sum:.2%} more intensity after padding", verbose=self.verbose)
+            vprint(f"Original meas int sum = {meas_int_sum:.4f}, padded region int sum = {padded_int_sum:.4f}, or {padded_int_sum/meas_int_sum:.2%} more intensity after padding. This percentage should be ideally less than 5%, or you should set a lower threshold to exclude more central region.", verbose=self.verbose)
             
             if mode == 'precompute':
                 canvas = np.zeros((meas.shape[0], *meas_padded.shape[1:]))
@@ -419,6 +419,7 @@ class Initializer:
         vprint(f"meausrements int. statistics (min, mean, max) = ({meas.min():.4f}, {meas.mean():.4f}, {meas.max():.4f})", verbose=self.verbose)
         vprint(f"measurements                      (N, Ky, Kx) = {meas.dtype}, {meas.shape}", verbose=self.verbose)
         self.init_variables['measurements'] = meas
+        self.init_variables['meas_avg_sum'] = meas.mean(0).sum() + padded_int_sum if self.init_params['exp_params']['meas_pad']['mode'] is not None else meas.mean(0).sum()
         vprint(" ", verbose=self.verbose)
         
     def init_probe(self):
@@ -487,19 +488,18 @@ class Initializer:
         # Normalizing probe intensity
         pmode_max = self.init_params['exp_params']['pmode_max']
         try:
-            meas = self.init_variables['measurements']
+            meas_avg_sum = self.init_variables['meas_avg_sum'] # meas.mean(0).sum(), or total intensity of the averaged diffraction pattern
         except KeyError:
             # If 'measurements' doesn't exist, initialize it
-            vprint("Warning: 'measurements' key not found. Initializing measurements for probe intensity normalization...", verbose=self.verbose)
-            self.init_measurements()
-            meas = self.init_variables['measurements']
+            vprint("Warning: 'meas_avg_sum' key not found. Initializing measurements for probe intensity normalization...", verbose=self.verbose)
+            meas_avg_sum = self.init_variables['meas_avg_sum']
             
         # Select pmode range and print summary
         probe = probe[:pmode_max]
-        probe = probe / (np.sum(np.abs(probe)**2)/np.sum(meas)*len(meas))**0.5 # Normalizing the probe_data so that the sum(|probe_data|**2) is the same with an averaged single DP
+        probe = probe / (np.sum(np.abs(probe)**2)/meas_avg_sum)**0.5 # Normalizing the probe_data so that the sum(|probe_data|**2) is the same with an averaged single DP
         probe = probe.astype('complex64')
         vprint(f"probe                         (pmode, Ny, Nx) = {probe.dtype}, {probe.shape}", verbose=self.verbose)
-        vprint(f"sum(|probe_data|**2) = {np.sum(np.abs(probe)**2):.2f}, while sum(meas)/len(meas) = {np.sum(meas)/len(meas):.2f}", verbose=self.verbose)
+        vprint(f"sum(|probe_data|**2) = {np.sum(np.abs(probe)**2):.2f}, while meas.mean(0).sum() = {meas_avg_sum:.2f}", verbose=self.verbose)
         self.init_variables['probe'] = probe
         vprint(" ", verbose=self.verbose)
    

ptyrad/models.py
@@ -82,6 +82,9 @@ class PtychoAD(torch.nn.Module):
     def __init__(self, init_variables, model_params, device='cuda', verbose=True):
         super(PtychoAD, self).__init__()
         with torch.no_grad():
+            
+            vprint('### Initializing PtychoAD model ###', verbose=verbose)
+            
             # Setup model behaviors
             self.device                 = device
             self.verbose                = verbose
@@ -128,7 +131,7 @@ class PtychoAD(torch.nn.Module):
             self.tilt_obj               = bool(self.lr_params['obj_tilts']        != 0 or torch.any(self.opt_obj_tilts))          # Set tilt_obj to True if lr_params['obj_tilts'] is not 0 or we have any none-zero tilt values
             self.shift_probes           = bool(self.lr_params['probe_pos_shifts'] != 0)                                           # Set shift_probes to True if lr_params['probe_pos_shifts'] is not 0
             self.change_thickness       = bool(self.lr_params['slice_thickness']  != 0)
-            self.probe_int_sum          = self.get_measurements().mean(0).sum() # This is only used for the `fix_probe_int`
+            self.probe_int_sum          = self.get_complex_probe_view().abs().pow(2).sum() # This is only used for the `fix_probe_int`
             self.loss_iters             = []
             self.iter_times             = []
             self.dz_iters               = []
@@ -146,7 +149,10 @@ class PtychoAD(torch.nn.Module):
                 'probe'           : self.opt_probe,
                 'probe_pos_shifts': self.opt_probe_pos_shifts}
             self.create_optimizable_params_dict(self.lr_params, self.verbose)
-    
+
+            vprint('### Done initializing PtychoAD model ###', verbose=verbose)
+            vprint(' ', verbose=verbose)
+            
     def get_complex_probe_view(self):
         """ Retrieve complex view of the probe """
         # This is a post-processing to ensure minimal code changes in PtyRAD for the DDP (multiGPU) via NCCL due to limited support for Complex value
@@ -351,7 +357,7 @@ class PtychoAD(torch.nn.Module):
     def get_measurements(self, indices=None):
         """ Get measurements for each position """
         # Return the selected measurements based on input indices
-        # If no indices are passed, return the entire measurements
+        # If no indices are passed, return the entire measurements ignoring any "on-the-fly" padding/resampling
         
         measurements = self.measurements
         device       = self.device
@@ -364,18 +370,21 @@ class PtychoAD(torch.nn.Module):
         
         if indices is not None:
             measurements = self.measurements[indices]
-        else:
+            
+            if self.meas_padded is not None:
+                canvas = torch.zeros((measurements.shape[0], *meas_padded.shape[-2:]), dtype=dtype, device=device)
+                canvas += meas_padded
+                canvas[..., pad_h1:pad_h2, pad_w1:pad_w2] = measurements # Replace the center part with the original meas
+                measurements = canvas
+            
+            if self.meas_scale_factors is not None and any(factor != 1 for factor in scale_factor):
+                measurements = torch.nn.functional.interpolate(measurements[None,], scale_factor=scale_factor, mode='bilinear')[0] # 2D interpolate requires 4D input (N, C, H, W)
+                measurements = measurements / prod(scale_factor) # This ensures the intensity scale and the integrated intensity are unchanged
+            
+        else: # Skip the "on-the-fly" operations so it won't throw any CUDA out-of-memory error. All typical PtyRAD usuage would pass get_measurements(batch) so this should be ok.
+            if self.meas_padded is not None or self.meas_scale_factors is not None:
+                vprint(f"WARNING: 'on-the-fly' measurements padding/resampling detected, but they are ignored because it may cause 'CUDA out-of-memory' when 'get_measurements()' is called without any indices. The original measurement with shape = {self.measurements.shape} is returned instead.")
             measurements = self.measurements
-        
-        if self.meas_padded is not None:
-            canvas = torch.zeros((measurements.shape[0], *meas_padded.shape[-2:]), dtype=dtype, device=device)
-            canvas += meas_padded
-            canvas[..., pad_h1:pad_h2, pad_w1:pad_w2] = measurements # Replace the center part with the original meas
-            measurements = canvas
-        
-        if self.meas_scale_factors is not None and any(factor != 1 for factor in scale_factor):
-            measurements = torch.nn.functional.interpolate(measurements[None,], scale_factor=scale_factor, mode='bilinear')[0] # 2D interpolate requires 4D input (N, C, H, W)
-            measurements = measurements / prod(scale_factor) # This ensures the intensity scale and the integrated intensity are unchanged
         
         return measurements
         